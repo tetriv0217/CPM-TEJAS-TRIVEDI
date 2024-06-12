ec7619f31cc807683bc0c3c9244e782c0c9cccf2
tetriv0217 <tetriv0217@gmail.com>
2024-06-12 13:17:17 +0530

    Added Dist

--- backend/src/dist/assets/fade-UwVswCAG.js
+++
@@ -1,4 +0,0 @@
-import{a2 as e}from"./index-BxX9oQzF.js";import{i as r}from"./IdurarOs-CQjgBJE3.js";const s=new e("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),m=new e("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),f=function(a){let i=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=a,n=`${o}-fade`,t=i?"&":"";return[r(n,s,m,a.motionDurationMid,i),{[`
-        ${t}${n}-enter,
-        ${t}${n}-appear
-      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{f as i};
---
+++ backend/src/dist/assets/useMail-T02mivop.js
@@ -0,0 +1,1 @@
+import{e as i}from"./actions-CqH49u7k.js";import{ai as n,aq as r}from"./index-Ceshqiaz.js";import{h as c}from"./selectors-DzM9jS4k.js";function d({entity:s}){const{isLoading:t}=n(c),a=r();return{send:o=>{const e={id:o};a(i.mail({entity:s,jsonData:e}))},isLoading:t}}export{d as u};
